2be4e70ba61754c90bdf7b031126668e87fda492
Joseph Coats <coats.joseph30@gmail.com>
2022-07-31 03:02:29 -0700

    practice

---
+++ fundamentals/fundamentals/Name/Name.java
@@ -0,0 +1,9 @@
+package fundamentals.fundamentals.Name;
+
+public class Name {
+    public static void main(String[] args) {
+        System.out.println("My name is Joseph Coats");
+        System.out.println("I am 26 years old");
+        System.out.println("My hometown is Little Rock Arkansas");
+    } 
+}
